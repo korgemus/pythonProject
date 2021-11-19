3429b2e122ea34afe59df943990b757f9a65fcde
korgemus <korgemus@yandex.ru>
2021-11-19 12:06:38 +0300

    Add draw circle

--- 255.py
+++ 255.py
@@ -1,26 +1,47 @@
 from turtle import *
 
 
-def circ(x):
-    circle(x)
+# def circ(x):
+#     circle(x)
+#
+# color('green')
+# begin_fill()
+#
+# a=list(map(int,input().split()))
+# max=a[0]
+# for w in a:
+#     if (w>max) & (w<=200):
+#         max=w
+# for i in a:
+#     if i>200:
+#         color('blue')
+#         circ(20)
+#     elif i==max:
+#         color('red')
+#         circ(i)
+#     else:
+#         color('green')
+#         circ(i)
+# exitonclick()
+# print(max)
+def qudr(x):
+    forward(x)
+    left(90)
+    forward(x)
+    left(90)
+    forward(x)
+    left(90)
+    forward(x)
+    left(90)
 
 color('green')
 begin_fill()
-
-a=list(map(int,input().split()))
-max=a[0]
-for w in a:
-    if (w>max) & (w<=200):
-        max=w
-for i in a:
-    if i>200:
-        color('blue')
-        circ(20)
-    elif i==max:
-        color('red')
-        circ(i)
-    else:
-        color('green')
-        circ(i)
+penup()
+setposition(-400,-380)
+pendown()
+m=int(input())
+c=int(input())
+for i in range(0,c):
+    qudr(m)
+    m=m/2
 exitonclick()
-print(max)
