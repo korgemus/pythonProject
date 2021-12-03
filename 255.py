3f840df3b40a8b0c92ee0ab2ec4dcdc0f6bcba75
korgemus <korgemus@yandex.ru>
2021-12-03 12:12:01 +0300

    count st

--- 255.py
+++ 255.py
@@ -1,16 +1,19 @@
 def sl():
+    global count_st
     _set = {}
     s = input()
     while s != '':
         _s = s.split()
         _set[_s[0]+' '+_s[1]] = int(_s[2])
         s = input()
+        count_st+=1
     return _set
 
 
 inf = dict()
 mat = dict()
 phys = dict()
+count_st=0
 
 print('Введите количество олимпиад')
 count = int(input())
@@ -20,3 +23,5 @@ for i in range(0, count):
 
 for _set in _list_dict:
     print(_set)
+
+print(f'Количество учеников:{count_st}')
