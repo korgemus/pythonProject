f13be3929c1aded4ae0b52583491e0dc9782dec1
korgemus <korgemus@yandex.ru>
2021-12-03 12:00:21 +0300

    List of olimpiads

--- 255.py
+++ 255.py
@@ -1,47 +1,22 @@
-from turtle import *
+def sl():
+    _set = {}
+    s = input()
+    while s != '':
+        _s = s.split()
+        _set[_s[0]+' '+_s[1]] = int(_s[2])
+        s = input()
+    return _set
 
 
-# def circ(x):
-#     circle(x)
-#
-# color('green')
-# begin_fill()
-#
-# a=list(map(int,input().split()))
-# max=a[0]
-# for w in a:
-#     if (w>max) & (w<=200):
-#         max=w
-# for i in a:
-#     if i>200:
-#         color('blue')
-#         circ(20)
-#     elif i==max:
-#         color('red')
-#         circ(i)
-#     else:
-#         color('green')
-#         circ(i)
-# exitonclick()
-# print(max)
-def qudr(x):
-    forward(x)
-    left(90)
-    forward(x)
-    left(90)
-    forward(x)
-    left(90)
-    forward(x)
-    left(90)
+inf = dict()
+mat = dict()
+phys = dict()
 
-color('green')
-begin_fill()
-penup()
-setposition(-400,-380)
-pendown()
-m=int(input())
-c=int(input())
-for i in range(0,c):
-    qudr(m)
-    m=m/2
-exitonclick()
+print('Введите количество олимпиад')
+count = int(input())
+_list_dict = [0]*count
+for i in range(0, count):
+    _list_dict[i] = sl()
+
+for _set in _list_dict:
+    print(_set)
